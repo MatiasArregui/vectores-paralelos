70b75a2e2d8b39c537800624bb29ee78871f1399
MatiasArregui <matiasarregui2@gmail.com>
2023-04-11 11:58:19 -0300

    ejercicio terminado

--- clases 1/archivo1.cpp
+++ clases 1/archivo1.cpp
@@ -1,64 +1,56 @@
 #include<iostream>
 
 using namespace std;
-
-//En un curso de 4 alumnos se registraron las notas de sus exámenes y se deben procesar de acuerdo a lo siguiente :
-//a) Ingresar Nombre y Nota de cada alumno(almacenar los datos en dos vectores paralelos)
-//b) Realizar un listado que muestre los nombres, notas y condición del alumno.En la condición, colocar "Muy Bueno" 
-//si la nota es mayor o igual a 8, "Bueno" si la nota está entre 4 y 7, y colocar "Insuficiente" si la nota es inferior a 4.
-//c) Imprimir cuantos alumnos tienen la leyenda “Muy Bueno”.
-
-class alumnos {
+//Cargar un vector de 5 elementos enteros.imprimir el menor y un mensaje si se repite dentro del vector.
+class elementos {
 private:
-	int notas[4];
-	char nombres[4][40];
+	int elemEnteros[5];
+	int menor;
+	int menorRepite(int posicion);
 public:
-	void cargaNotasNombre();
-	void listarCondiciones();
+	void carga();
+	void buscarMenor();
+
 };
-void alumnos::cargaNotasNombre() {
-	for (int i = 0; i < 4; i++)
+void elementos::carga() {
+	for (int i = 0; i < 5; i++)
 	{
-		cout << "Ingrese el nombre del alumno:\n";
-		cin.getline(nombres[i], 40);
-		cout << "\nIngrese la nota del alumno:\n";
-		cin >> notas[i];
+		cout << "\nIngrese el valor del entero: ";
+		cin >> elemEnteros[i];
 		cin.get();
 	}
 }
-void alumnos::listarCondiciones() {
-	int muyBueno = 0;
-	for (int i = 0; i < 4; i++)
+void elementos::buscarMenor() {
+	menor = elemEnteros[0];
+	int pos = 0;
+	for (int i = 0; i < 5; i++)
 	{
-		if (notas[i]>=8)
+		if (elemEnteros[i]<menor)
 		{
-			cout << "\nAlmuno: " << nombres[i];
-			cout << "\nNota: " << notas[i];
-			cout << "\nCondicion: Muy Bueno";
-			muyBueno += 1;
-		}
-		else
-		{
-			if (4<=notas[i]<=7)
-			{
-				cout << "\nAlmuno: " << nombres[i];
-				cout << "\nNota: " << notas[i];
-				cout << "\nCondicion: Bueno";
-			}
-			else
-			{
-				cout << "\nAlmuno: " << nombres[i];
-				cout << "\nNota: " << notas[i];
-				cout << "\nCondicion: Insuficiente";
-			}
+			menor = elemEnteros[i];
+			pos = i;
 		}
 	}
-	cout << "\nAlumnos que tienen una condicion Muy Buena: " << muyBueno;
+	cout << "\nEl menor valor es: " << menor;
+	int repite = menorRepite(pos);
+	cout << "\nEl menor se repite: " << repite << " veces en el array";
 
 }
+int elementos::menorRepite(int posicion) {
+	int repite = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		if (elemEnteros[posicion]==elemEnteros[i] && i !=posicion)
+		{
+			repite += 1;
+		}
+	}
+	return repite;
+}
 int main() {
-	alumnos pAno1;
-	pAno1.cargaNotasNombre();
-	pAno1.listarCondiciones();
+	elementos e1;
+	e1.carga();
+	e1.buscarMenor();
+
 	return 0;
 }
